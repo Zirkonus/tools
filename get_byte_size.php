214bbcf7092fbcbb1662b6f67fe85dbe641f8347
Aleksei Dovzhenko <alex@gypost.com>
2016-03-24 12:05:55 +0200

    A method of number conversion between random scales of notation

---
+++ get_byte_size.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * A method of byte info displaying in an easy-to-read form
+ * 
+ * @author Oleksii Dovzhenko <zirkonus@gmail.com>
+ * @copyright 2010
+ * @license http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+
+function get_byte_size($size) {
+	if ($size < 1024) {
+		return intval($size) .' B';
+	} elseif ($size < 1048576) {
+		return round($size / 1024, 1) .' KB';
+	} elseif ($size < 1073741824) {
+		return round($size / 1048576, 1) . ' MB';
+	} elseif ($size < 1099511627776) {
+		return round($size / 1073741824, 1) . ' GB';
+	} else {
+		return round($size / 1099511627776, 1) .' TB';
+	}
+	return $size;
+}
+?>
